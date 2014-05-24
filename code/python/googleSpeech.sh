bf95fbb2ea4f6dd4a61dca65b8ef8e13fdcb9236
fitosegrera <fitosegrera@gmail.com>
2014-05-24 05:43:35 -0400

    advances in python... recording using a shell script that executes sox

--- code/python/googleSpeech.sh
+++ code/python/googleSpeech.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Recording... Press Ctrl+C to Stop."
-ffmpeg -f alsa -ac 2 -ar 44100 -i pulse voice.flac 
+#This script is executed from python and uses SOX to record a .flac audio file from the microphone.
+#It starts recording after detecting at least 0.1 seconds of noise and stops after 1.5 second of silence. 
+#Sensitivity is set to 1%.
+
+echo "Recording..."
+sox -r 16000 -t alsa default voice.flac silence 1 0.1 1% 1 1.5 1%
+
+
 
 	
--- code/python/ouijaBoard_2.0.py
+++ code/python/ouijaBoard_2.0.py
@@ -12,28 +12,19 @@ while True:
     #print 'You pressed %s' % char
     if char == 'r':
     	subprocess.call(['./googleSpeech.sh'])
-    	print "capturing..."
-		
+    	print "/////////////////"
 
-print "type the word 'start' followed by ENTER to start the convresion to text..."
+    	f = open("voice.flac")
+    	flac_cont = f.read()
+    	f.close()
 
-while True:
-    char = sys.stdin.read(1)
-    #print 'You pressed %s' % char
-    if char == 'start':
-    	print "converting speech to text..."
-
-'''
-f = open("voice.flac")
-flac_cont = f.read()
-f.close()
-
-lang_code='en-US'
-googl_speech_url = 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw'
-hrs = {'Content-type': 'audio/x-flac; rate=44100'}
-req = urllib2.Request(googl_speech_url, data=flac_cont, headers=hrs)
-p = urllib2.urlopen(req)
-print p.read()
-
-#os.remove('ouija.flac') #This line deletes the audio file once the operation is done
-'''
+    	lang_code='en-US'
+    	googl_speech_url = 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw'
+    	hrs = {'Content-type': 'audio/x-flac; rate=16000'}
+    	req = urllib2.Request(googl_speech_url, data=flac_cont, headers=hrs)
+    	p = urllib2.urlopen(req)
+    	print p.read()
+    	#j = json.load(p)
+    	#ourResult = j['result'][0]['alternative']
+    	#print ourResult
+    	#os.remove('voice.flac') #This line deletes the audio file once the operation is done
